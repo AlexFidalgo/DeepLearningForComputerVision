272e75e5cfe83afab6fde34fb9807f2c3eae0f73
AlexFidalgo <alexfidalgo10@gmail.com>
2024-04-23 22:16:48 -0300

    starts qnn_kd

--- aula05/aprendizado_maquina_para_filtros/qnn_kd.py
+++ aula05/aprendizado_maquina_para_filtros/qnn_kd.py
@@ -1,10 +1,69 @@
-algorithm = 'kd_tree'
-
 import numpy as np
 from sklearn.neighbors import KDTree
-rng = np.random.RandomState(0)
-X = rng.random_sample((10, 3))  # 10 points in 3 dimensions
-tree = KDTree(X, leaf_size=2)              
-dist, ind = tree.query(X[:1], k=3)                
-print(ind)  # indices of 3 closest neighbors
-print(dist)  # distances to 3 closest neighbors
+import cv2
+import os
+import time
+import numpy as np
+
+def reverse_transform_index(index, A):
+
+    l, c = A.shape
+    l_index = index // c
+    c_index = index % c
+    return l_index, c_index
+
+
+def get_vizinhanca_matrix(A):
+
+    l, c = A.shape
+    C = np.zeros((l * c, 9), dtype=np.uint8)
+
+    for i in range(l):
+        for j in range(c):
+            index = i * c + j
+            if i < 1 or i >= l - 1 or j < 1 or j >= c - 1:
+                C[index] = np.full((1, 9), 255, dtype=np.uint8)
+            else:
+                C[index] = vizinhanca33(AX, i, j)
+
+    return C
+
+def vizinhanca33(a, lc, cc):
+    d = np.zeros((1, 9), dtype=np.uint8)
+    i = 0
+    for l in range(-1, 2):
+        for c in range(-1, 2):
+            d[0, i] = a[lc + l, cc + c]
+            i += 1
+    return d
+
+def NearestNeighbors(AX, AY, QX):
+
+    viz_AX = get_vizinhanca_matrix(AX)
+    viz_QX = get_vizinhanca_matrix(QX)
+
+    QP = []
+
+    tree = KDTree(viz_AX)
+
+    for row in viz_QX:
+
+        ind = tree.query(row.reshape(1,-1))[1]
+        index = reverse_transform_index(ind[0,0], AX)
+        QP.append(AY[index])
+
+    return QP
+
+
+if __name__ == "__main__":
+    current_directory = os.path.abspath(os.path.dirname(__file__))
+    AX = cv2.imread(os.path.join(current_directory, "ax.bmp"), 0)
+    AY = cv2.imread(os.path.join(current_directory, "ay.bmp"), 0)
+    QX = cv2.imread(os.path.join(current_directory, "qx.bmp"), 0)
+    
+    start_time = time.time()
+    result = NearestNeighbors(AX, AY, QX)
+    end_time = time.time()
+    print("Execution time: {:.2f} seconds".format(end_time - start_time))
+    
+    cv2.imwrite(os.path.join(current_directory, "x_kd.pgm"), result)
